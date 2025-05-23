9d29d6ac3cbb8ac0589c74d87e2d650e00de631d
CarlosEducg11 <carlos.goncalves@sou.unifeob.edu.br>
2025-05-23 11:49:36 -0300

    c

--- python-generator/script.py
+++ python-generator/script.py
@@ -17,7 +17,7 @@ solos = ['Arenoso', 'Arenoso', 'Arenoso', 'Argiloso', 'Argiloso', 'Humífero',
          'arenoso', 'ARENOSO', 'arenoso', 'argiloso', 'ARGILOSO', 'humifero']
 notas = ['Queimada Recente', 'Historicamente alagavel', 'Mare alta']
 
-for idRio in range(1000):
+for idRio in range(20):
     mediaVazao = round(random.triangular(2000, 200000, 5000))
 
     if random.random() < 0.05:
